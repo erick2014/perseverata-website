a09e8da2f4b9f5ac06b45c51dcd99a583de57c1c
erick <erickarmandogarcia@hotmail.com>
2017-04-29 17:35:03 -0500

    Remove previous folder structure

--- webpack.dev.config.js
+++
@@ -1,3 +0,0 @@
-var webpack = require("webpack");
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-module.exports = require('./webpack.config.js');    // inherit from the main config file
